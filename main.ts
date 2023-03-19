bd92f9425d46efd4b086f3b69da286771abfca8b
davidananas <116786922+davidananas@users.noreply.github.com>
2023-03-19 17:16:33 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,55 @@
+input.onPinPressed(TouchPin.P0, function () {
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
+    basic.showString("ZAPLATTE 10 KC")
+})
+input.onButtonPressed(Button.A, function () {
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.showArrow(ArrowNames.East)
+    basic.clearScreen()
+    basic.showString("VODA")
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.East)
+    basic.clearScreen()
+    basic.showString("LED")
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
+    basic.showString("ZAPLATTE 10 KC")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("VODA")
+    basic.clearScreen()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.clearScreen()
+    basic.showString("LED")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("VODA")
+})
 input.onPinPressed(TouchPin.P1, function () {
     music.playTone(392, music.beat(BeatFraction.Whole))
     music.playTone(523, music.beat(BeatFraction.Double))
     basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
     basic.showString("ZAPLACENO")
-})
-basic.forever(function () {
-    while (input.buttonIsPressed(Button.A)) {
-        music.playTone(523, music.beat(BeatFraction.Whole))
-        basic.showArrow(ArrowNames.West)
-        basic.clearScreen()
-        basic.showString("ZAPLATTE 10 KC")
-    }
-    while (input.buttonIsPressed(Button.B)) {
-        music.playTone(523, music.beat(BeatFraction.Whole))
-        basic.showArrow(ArrowNames.West)
-        basic.clearScreen()
-        basic.showString("ZAPLATTE 10 KC")
-    }
 })
