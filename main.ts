0fdc61d87100a196334c7e47bf2cfd417b39ba14
davidananas <116786922+davidananas@users.noreply.github.com>
2023-03-19 17:25:34 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,15 @@
 input.onPinPressed(TouchPin.P0, function () {
-    music.playTone(523, music.beat(BeatFraction.Whole))
     basic.showArrow(ArrowNames.West)
     basic.clearScreen()
     basic.showString("ZAPLATTE 10 KC")
 })
 input.onButtonPressed(Button.A, function () {
-    music.playTone(523, music.beat(BeatFraction.Whole))
+    basic.clearScreen()
+    basic.showArrow(ArrowNames.West)
+    basic.clearScreen()
     basic.showString("VODA")
     basic.clearScreen()
     basic.showArrow(ArrowNames.East)
-    basic.clearScreen()
-    basic.showString("LED")
-    basic.clearScreen()
-    basic.showArrow(ArrowNames.West)
     basic.clearScreen()
     basic.showLeds(`
         . . # . .
@@ -22,8 +19,9 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         `)
     basic.clearScreen()
-    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.West)
     basic.clearScreen()
+    basic.showString("LED")
 })
 input.onPinPressed(TouchPin.P2, function () {
     basic.showArrow(ArrowNames.West)
@@ -48,8 +46,6 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("VODA")
 })
 input.onPinPressed(TouchPin.P1, function () {
-    music.playTone(392, music.beat(BeatFraction.Whole))
-    music.playTone(523, music.beat(BeatFraction.Double))
     basic.showIcon(IconNames.Happy)
     basic.clearScreen()
     basic.showString("ZAPLACENO")
