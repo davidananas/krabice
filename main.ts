f4d3154dae7b9904fa74888d1e529cb202090251
davidananas <116786922+davidananas@users.noreply.github.com>
2023-03-19 16:45:07 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onPinPressed(TouchPin.P1, function () {
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Double))
+    basic.showIcon(IconNames.Happy)
+    basic.showString("ZAPLACENO")
+})
 basic.forever(function () {
-	
+    while (input.buttonIsPressed(Button.A)) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        basic.showArrow(ArrowNames.West)
+        basic.clearScreen()
+        basic.showString("ZAPLATTE 10 KC")
+    }
+    while (input.buttonIsPressed(Button.B)) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+        basic.showArrow(ArrowNames.West)
+        basic.clearScreen()
+        basic.showString("ZAPLATTE 10 KC")
+    }
 })
